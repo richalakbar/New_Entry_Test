584f09847b3d688a8123fa8b7867175405d9e03c
Rezsss <71911982+kurniawan-rgb@users.noreply.github.com>
2024-06-11 11:09:44 +0300

    update admin user

--- src/views/adminUser.vue
+++ src/views/adminUser.vue
@@ -20,81 +20,31 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(user, index) in users" :key="index">
+            <tr v-for="(user, index) in users" :key="user.id">
               <th class="text-center th-middle" scope="row">{{ index + 1 }}</th>
-              <td v-if="!user.isNew && !user.isEditing" class="text-left td-middle">
-                {{ user.name }}
-              </td>
-              <td v-else><input v-model="user.name" class="table-input" /></td>
-              <td v-if="!user.isNew && !user.isEditing" class="text-left td-middle">
-                {{ user.fakultas }}
-              </td>
-              <td v-else>
-                <input v-model="user.fakultas" class="table-input" />
-              </td>
-              <td v-if="!user.isNew && !user.isEditing" class="text-left td-middle">
-                {{ user.prodi }}
-              </td>
-              <td v-else><input v-model="user.prodi" class="table-input" /></td>
-              <td v-if="!user.isNew && !user.isEditing" class="text-left td-middle">
-                {{ user.username }}
-              </td>
-              <td v-else>
-                <input v-model="user.username" class="table-input" />
-              </td>
-              <td v-if="!user.isNew && !user.isEditing" class="text-left td-middle">
-                {{ user.password }}
-              </td>
-              <td v-else>
-                <input v-model="user.password" class="table-input" />
-              </td>
-              <td class="text-center td-middle">
-                <iconPlus v-if="!user.isNew && !user.isEditing" />
-              </td>
-              <td class="text-center td-middle">
-                <iconPlus v-if="!user.isNew && !user.isEditing" />
-              </td>
-              <td class="text-center td-middle">
-                <iconPlus v-if="!user.isNew && !user.isEditing" />
-              </td>
-              <td v-if="user.isNew" class="text-center action-column">
-                <button @click="addUser(index)" class="action-btn add-btn"> 
-                  Tambah
-                </button>
-              </td>
-              <td v-else class="text-center action-column">
-                <button
-                  @click="editUser(index)"
-                  v-if="!user.isEditing"
-                  class="action-btn edit-btn"
-                  style="margin-right: 5px;"
-                >
-                  Edit
-                </button>
-                <button
-                  @click="saveUser(index)"
-                  v-else
-                  class="action-btn save-btn"
-                  style="margin-right: 5px"
-                >
-                  Simpan
-                </button>
-                <button
-                  @click="removeUser(index)"
-                  class="action-btn remove-btn"
-                >
-                  Hapus
-                </button>
+              <td v-if="!user.isEditing" class="text-left td-middle">{{ user.nama_lengkap }}</td>
+              <td v-else><input v-model="user.nama_lengkap" class="table-input" /></td>
+              <td v-if="!user.isEditing" class="text-left td-middle">{{ user.fakultas }}</td>
+              <td v-else><input v-model="user.fakultas" class="table-input" /></td>
+              <td v-if="!user.isEditing" class="text-left td-middle">{{ user.program_studi }}</td>
+              <td v-else><input v-model="user.program_studi" class="table-input" /></td>
+              <td v-if="!user.isEditing" class="text-left td-middle">{{ user.username }}</td>
+              <td v-else><input v-model="user.username" class="table-input" /></td>
+              <td v-if="!user.isEditing" class="text-left td-middle">{{ user.password }}</td>
+              <td v-else><input v-model="user.password" class="table-input" /></td>
+              <td class="text-center td-middle"><iconPlus v-if="!user.isEditing" /></td>
+              <td class="text-center td-middle"><iconPlus v-if="!user.isEditing" /></td>
+              <td class="text-center td-middle"><iconPlus v-if="!user.isEditing" /></td>
+              <td class="text-center action-column">
+                <button v-if="!user.isEditing" @click="editUser(index)" class="action-btn edit-btn">Edit</button>
+                <button v-else @click="saveUser(index)" class="action-btn save-btn">Simpan</button>
+                <button @click="removeUser(user.id)" style="margin-left: 5px" class="action-btn remove-btn">Hapus</button>
               </td>
             </tr>
           </tbody>
         </table>
         <div>
-          <router-link to="/create">
-            <button class="action-btn add-new-btn">
-              Tambah User
-            </button>
-          </router-link>
+          <button @click="addNewRow" class="action-btn add-new-btn">Tambah User</button>
         </div>
       </div>
     </main>
@@ -102,6 +52,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+import api from '../services/api';
 import navbarPage from "@/components/navbarComponent.vue";
 import navbarAdmin from "@/components/navbarAdmin.vue";
 import iconPlus from "@/components/iconPlus.vue";
@@ -110,220 +62,85 @@ export default {
   components: {
     navbarPage,
     navbarAdmin,
-    iconPlus,
+    iconPlus
   },
   data() {
     return {
-      users: [
-        {
-          name: "Richal Akbar",
-          fakultas: "Teknik",
-          prodi: "Teknik Informatika",
-          username: "d121211084",
-          password: "d1212110841967",
-          isNew: false,
-          isEditing: false,
-        },
-        {
-          name: "Andi Muhammad Sabar",
-          fakultas: "Teknik",
-          prodi: "Teknik Informatika",
-          username: "d121211084",
-          password: "d1212110841967",
-          isNew: false,
-          isEditing: false,
-        },
-        {
-          name: "Reza Kurniawan",
-          fakultas: "Teknik",
-          prodi: "Teknik Informatika",
-          username: "d121211084",
-          password: "d1212110841967",
-          isNew: false,
-          isEditing: false,
-        },
-      ],
+      users: []
     };
   },
+  created() {
+    this.fetchUsers();
+  },
   methods: {
+    async fetchUsers() {
+      try {
+        const response = await api.getUsers();
+        this.users = response.data.map(user => ({ ...user, isEditing: false }));
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    },
     addNewRow() {
-      // Metode ini sekarang dinonaktifkan dan tidak akan dipanggil
-    },
-    addUser(index) {
-      this.users[index].isNew = false;
-    },
-    editUser(index) {
-      this.users.forEach((user, i) => {
-        if (i === index) {
-          user.isEditing = true;
-        } else {
-          user.isEditing = false;
-        }
+      this.users.push({
+        id: null,
+        nama_lengkap: '',
+        fakultas: '',
+        program_studi: '',
+        username: '',
+        password: '',
+        isEditing: true
       });
     },
-    saveUser(index) {
-      this.users[index].isEditing = false;
+    async addUser(index) {
+      const user = this.users[index];
+      try {
+        const response = await api.createUser(user);
+        this.$set(this.users, index, { ...response.data, isEditing: false });
+      } catch (error) {
+        console.error('Error adding user:', error);
+      }
     },
-    removeUser(index) {
-      this.users.splice(index, 1);
+    editUser(index) {
+      this.$set(this.users, index, { ...this.users[index], isEditing: true });
     },
-  },
+    async saveUser(index) {
+      const user = this.users[index];
+      try {
+        if (user.id) {
+          const response = await api.updateUser(user.id, user);
+          this.$set(this.users, index, { ...response.data, isEditing: false });
+        } else {
+          const response = await api.createUser(user);
+          this.$set(this.users, index, { ...response.data, isEditing: false });
+        }
+      } catch (error) {
+        console.error('Error saving user:', error);
+      }
+    },
+    async removeUser(id) {
+      try {
+        await api.deleteUser(id);
+        this.users = this.users.filter(user => user.id !== id);
+      } catch (error) {
+        console.error('Error removing user:', error);
+      }
+    }
+  }
 };
 </script>
 
-<style>
-.container-admin-user {
-  padding: 20px;
+<style scoped>
+/* Add your styles here */
+.form-group {
+  margin-bottom: 15px;
 }
-
-.container-admin-user {
-  display: flex;
-}
-
-.custom-nav-admin {
-  width: 250px;
-}
-
-.content-admin-user {
-  flex-grow: 1;
-  padding: 20px;
-}
-
-.table {
+.form-control {
   width: 100%;
-  border-collapse: collapse;
-  margin-top: 20px;
+  padding: 10px;
+  margin-top: 5px;
 }
-
-.col-no {
-  width: 50px;
-}
-
-.table th,
-.table td {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-.table th {
-  background-color: #edfdfa;
-  text-align: left;
-}
-
-.question-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.icon-button {
-  margin-left: 10px;
-}
-
-.form-check {
-  display: flex;
-  justify-content: center;
-}
-
-.form-check-input {
-  background-color: #f2f2f2;
-  width: 20px;
-  height: 20px;
-  margin: center;
-  border: 2px solid #d0cece;
-  border-radius: 5px;
-}
-
-.table {
-  width: 100%;
-  margin-bottom: 20px;
-  border-collapse: collapse;
-}
-
-.table th,
-.table td {
-  border: 1px solid #dee2e6;
-  padding: 8px;
-  vertical-align: middle;
-}
-
-.table-input {
-  width: 100%;
-  box-sizing: border-box;
-  padding: 6px 12px;
-  border: 1px solid #ced4da;
-  border-radius: 4px;
-}
-
-.action-btn {
-  line-height: 1;
-  background-color: #2aaa94;
-  color: white;
-  border: none;
-  padding: 8px 16px;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 14px;
-  width: auto;
-}
-
-.action-btn:hover {
-  background-color: #249e88;
-}
-
-.action-btn:active {
-  background-color: #208a76;
-}
-
-.action-btn:focus {
-  outline: none;
-}
-
-.remove-btn {
-  background-color: #e74c3c;
-  line-height: 1;
-}
-
-.remove-btn:hover {
-  background-color: #c0392b;
-}
-
-.remove-btn:active {
-  background-color: #922b21;
-}
-
-.text-left {
-  text-align: left;
-}
-
-.text-center {
-  text-align: center;
-}
-
-.button-container {
-  text-align: center;
-}
-
-.edit-btn {
-  background-color: blue;
-  padding: 8px 16px;
-  line-height: 1;
-}
-
-.edit-btn:hover {
-  background-color: darkblue;
-}
-
-.edit-btn:active {
-  background-color: navy;
-}
-
-.td-middle,
-.th-middle {
-  vertical-align: middle;
-}
-
-.action-column {
-  width: 20ch; 
+.btn {
+  margin-top: 10px;
 }
 </style>
